1cd11e19a15cea192c0938b22786188abe140582
jimenglish81 <jimenglish81@gmail.com>
2016-10-02 15:15:54 +0100

    expiry tweak

--- src/js/reducers/authentication.js
+++ src/js/reducers/authentication.js
@@ -27,7 +27,7 @@ export default (state={ isAuthenticated: false }, { payload, type }) => {
       return {
         isAuthenticated: false,
         session: null,
-        error: payload || 'generic error',
+        error: payload || 'Sign in failed.',
         loading: false,
       };
     case UNAUTH_SUCCESS:
