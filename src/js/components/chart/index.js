2628a248cb23add08d6f14c0312fe68fdef6b4be
jimenglish81 <jimenglish81@gmail.com>
2016-10-23 19:39:37 +0100

    make chart responsive

--- src/js/components/chart/index.js
+++ src/js/components/chart/index.js
@@ -10,13 +10,16 @@ class Chart extends Component {
 
     this.state = {
       parentWidth: 0,
+      parentHeight: 0,
     };
     this.debounceWindowResize = _.debounce(this.windowResize.bind(this), 150);
   }
 
   windowResize() {
+    const { parentNode } = findDOMNode(this);
     this.setState({
-       parentWidth: findDOMNode(this).parentNode.offsetWidth,
+       parentWidth: parentNode.offsetWidth,
+       parentHeight: parentNode.offsetHeight,
     });
   }
 
@@ -38,9 +41,13 @@ class Chart extends Component {
   }
 
   render() {
-    const margin = { top: 20, right: 100, bottom: 40, left: 40 };
+    const isNarrow = this.state.parentWidth <= 570;
+    const margin = !isNarrow ?
+      { top: 20, right: 80, bottom: 40, left: 40 }
+      :
+      { top: 10, right: 10, bottom: 10, left: 10 };
     const width = this.state.parentWidth - (margin.left + margin.right);
-    const height = 400 - (margin.top + margin.bottom);
+    const height = this.state.parentHeight - (margin.top + margin.bottom);
     const data = this.props.dataPoints.map(({ timestamp, price }) => ({
       timestamp: d3.timeParse('%Y/%m/%d %H:%M:%S')(timestamp),
       price,
@@ -69,31 +76,33 @@ class Chart extends Component {
       .range([height, 0])
       .domain([min - padding, max + padding]);
 
-    const yAxis = d3.axisRight()
-      .scale(yScale)
-      .tickSize(-width, 0, 0);
+    if (!isNarrow) {
+      const yAxis = d3.axisRight()
+        .scale(yScale)
+        .tickSize(-width, 0, 0);
 
-    const xAxis = d3.axisBottom()
-      .ticks(d3.timeSecond, 10)
-      .scale(xScale)
-      .tickFormat(d3.timeFormat('%H:%M:%S'));
+      const xAxis = d3.axisBottom()
+        .ticks(d3.timeSecond, 10)
+        .scale(xScale)
+        .tickFormat(d3.timeFormat('%H:%M:%S'));
 
-    el.append('g')
-      .attr('className', 'sparkline')
-    	.attr('transform', `translate(${width}, 0)`)
-    	.call(yAxis)
+      el.append('g')
+        .attr('className', 'sparkline')
+      	.attr('transform', `translate(${width}, 0)`)
+      	.call(yAxis)
 
-    el.append('g')
-      .attr('className', 'sparkline')
-      .attr('transform', `translate(0, ${(height)})`)
-      .call(xAxis)
-      .selectAll('text')
-      .attr('font-size', '9px')
-      .attr('y', 0)
-      .attr('x', 9)
-      .attr('dy', '.35em')
-      .attr('transform', 'rotate(45)')
-      .style('text-anchor', 'start');
+      el.append('g')
+        .attr('className', 'sparkline')
+        .attr('transform', `translate(0, ${(height)})`)
+        .call(xAxis)
+        .selectAll('text')
+        .attr('font-size', '9px')
+        .attr('y', 0)
+        .attr('x', 9)
+        .attr('dy', '.35em')
+        .attr('transform', 'rotate(45)')
+        .style('text-anchor', 'start');
+    }
 
     const line = d3.line()
       .x(function (d) {
@@ -113,14 +122,16 @@ class Chart extends Component {
     // width should be baed on position.expiryTime or last timestamp (whicheven shorter)
     // could be a selectedPsotion if chart gets too budy
     // hover on line then shows price
-    el.append('line')
-       .attr('y1', yScale( _.get(_.last(data), 'price')))
-       .attr('y2', yScale( _.get(_.last(data), 'price')))
-       .attr('x1', 0)
-       .attr('x2', xScale(_.get(_.last(data), 'timestamp')))
-       .attr('stroke-width', 1)
-       .attr('opacity', 0.6)
-       .attr('stroke', '#1997c6');
+    if (isNarrow) {
+      el.append('line')
+         .attr('y1', yScale( _.get(_.last(data), 'price')))
+         .attr('y2', yScale( _.get(_.last(data), 'price')))
+         .attr('x1', 0)
+         .attr('x2', xScale(_.get(_.last(data), 'timestamp')))
+         .attr('stroke-width', 1)
+         .attr('opacity', 0.6)
+         .attr('stroke', '#1997c6');
+    }
 
     const lastValue = _.get(_.last(data), 'price');
     const g = el.append('g')
