11b50d8a028cb9dd2cb382c45c94bc9b55853bb0
jimenglish81 <jimenglish81@gmail.com>
2016-10-23 17:36:09 +0100

    start on responsiveness

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,17 +3,20 @@ import React, { PropTypes } from 'react';
 const Header = (props) => {
   return (
     <nav className="nav">
-      <svg
-        fill="#1997c6"
-        height="24"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0h24v24H0z" fill="none"/>
-        <path d="M7 2v11h3v9l7-12h-4l4-8z"/>
-      </svg>
       <h2 className="nav__logo">
-        FastMarkets
+        <svg
+          fill="#1997c6"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h24v24H0z" fill="none"/>
+          <path d="M7 2v11h3v9l7-12h-4l4-8z"/>
+        </svg>
+        <span className="nav__title">
+          <span className="nav__title--small">FM</span>
+          <span className="nav__title--large">FastMarkets</span>
+        </span>
       </h2>
       {props.children}
     </nav>
--- src/js/containers/header.js
+++ src/js/containers/header.js
@@ -25,18 +25,29 @@ class Header extends Component {
 
     if (isAuthenticated) {
       return (
-        <ul className="nav__navbar">
-          <li className="nav__navbar__item" key={1}>
-            <div className="nav__navbar__item__header">available</div>
+        <ul className="nav__nav-bar">
+          <li className="nav__nav-bar-item" key={1}>
+            <div className="nav__nav-bar-item-header">available</div>
             <span>{formatCurrency(availableCash, currency)}</span>
           </li>
-          <li className="nav__navbar__item" key={2}>
-            <div className="nav__navbar__item__header">profit/loss</div>
+          <li className="nav__nav-bar-item" key={2}>
+            <div className="nav__nav-bar-item-header">profit/loss</div>
             <span>{formatCurrency(profitLoss, currency)}</span>
           </li>
-          <li className="nav__navbar__item" key={3}>
+          <li className="nav__nav-bar-item" key={3}>
             <button className="btn btn--slim btn--secondary" onClick={this._handleLogout}>
-              Sign out
+              <span className="nav__signout--small">
+                <svg fill="#f0f0f0" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
+                  />
+                  <path
+                    d="M0 0h24v24H0z"
+                    fill="none"
+                  />
+                </svg>
+              </span>
+              <span className="nav__signout--large">Sign out</span>
             </button>
           </li>
         </ul>
